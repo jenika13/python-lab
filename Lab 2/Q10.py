996f5f03778eeb653c623af7579854b6b32f8e56
Jenika B Pradeep <jenikab13@gmail.com>
2022-04-15 03:25:05 +0530

    Lab 2/Q10.py

--- Lab 2/Q10.py
+++
@@ -1,9 +0,0 @@
-#to reverse a tuple
-
-from audioop import reverse
-
-
-tuple_1 = ("apple","orange","mango","cherry","strawberry","grapes","banana","pineapple")
-
-x=reversed(tuple_1)
-print(tuple(x))
